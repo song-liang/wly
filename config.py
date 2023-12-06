a27404df8d905a036402b044f43b78f46450a39f
song-liang <1107179995@qq.com>
2023-12-06 21:59:30 +0800

    添加自动打开浏览器、登录、执行任务，配置文件与执行文件分开

---
+++ config.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# LiangLiang  2023.12.6
+
+# 配置文件，与用户名
+
+username = "xxxxx"
+password = "xxxxx"
+
+# 登录
+# http://wly.lequ.com/list/index.html
+login_url = "http://api.wly.lequ.com/mobile/login"
+
+# 游戏角色url
+# 魏
+role_weiwu = ("http://api.wly.lequ.com/mobile/enterserver/serverid/xxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+
+# 蜀
+role_linghu = ("http://api.wly.lequ.com/mobile/enterserver/serverid/xxxxxxxxxxxxxxxxxxxxxxxxxxxi")
+
+# 吴
+role_guihai = ("http://api.wly.lequ.com/mobile/enterserver/serverid/xxxxxxxxxxxxxxxxxxxxxxxxxxxx")
